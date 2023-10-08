0be05f019b5c28ce3e06dd2e442d810a8e46d0b8
Luiz Henrique <102159670+LuizHengs@users.noreply.github.com>
2023-10-07 21:55:48 -0300

    Servidor e Cliente locais
    Funcionam com 127.0.0.1 (localhost)

---
+++ Projeto_Final/Cliente.py
@@ -0,0 +1,78 @@
+import socket
+import sys
+
+SERVER_POT = 8000
+BUFFER = 1024
+
+def connecting():
+
+    value = input("Coloque o endereço de IP para começar a comunicaçao: ")
+    confirmation = input(f"\nO endereço de destino e {value}. E isso mesmo?\n ('s' ou 'n'): ")
+
+    confirmation = (False if confirmation == 'n' else True)
+    if confirmation is False:
+        print("Saindo...")
+        sys.exit()
+
+    return start_connection(value)
+
+def checking_ip_address (ip_address):
+    if len(ip_address) == 9 and ip_address is not None:
+        return True
+    print("Terminando o programa... cheque se o endereço de IP foi corrigido!")
+    sys.exit()
+
+def start_connection(ip_address):
+
+    print("Tentando se conectar ao servidor...")
+    checking_ip_address(ip_address)
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.SOCK_STREAM eh para conexao TCP. Para realizar UDP, use socket.SOCK_DGRAM
+
+    try:
+        destino = (ip_address,SERVER_POT)
+        connection.connect(destino)
+    except ConnectionError as erro:
+        print("Conexao recusada. "
+              f"Tipo do erro: {type(erro)}")
+
+    return connection
+
+def close_connection(connection):
+    print("Terminando a conexao TCP...")
+    connection.close()
+
+def conversation(connection):
+    print("Começando o chat! Para sair, escreva: 'sair'")
+
+    while True:
+        mensagem = input("\nVoce: ")
+
+        if mensagem != "":
+            connection.send(bytes(mensagem, "utf8"))
+            if mensagem == "sair":
+                break
+
+        rec_mensagem = connection.recv(BUFFER).decode("ascii")
+
+        if rec_mensagem != "":
+            print(f"Servidor: {rec_mensagem}")
+            if rec_mensagem == "sair":
+                print("O lado do servidor terminou a conexao. Informe 'sair' para terminar a conexao")
+
+    print("Saindo...")
+    close_connection(connection)
+
+if __name__ == '__main__':
+    print("Bem vindo ao chat com comunicaçao socket!")
+
+    conexao = connecting()
+    conversation(conexao)
+
+    try:
+        conexao.close()
+    except ConnectionError as erro:
+        print("A conexao TCP chegou ao fim")
+
+
+
+
---
+++ Projeto_Final/Servidor.py
@@ -0,0 +1,57 @@
+import socket
+import sys
+
+SERVER_IP = ''
+SERVER_POT = 8000
+BUFFER = 1024
+
+def bind_to_the_server():
+
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_server = (SERVER_IP, SERVER_POT)
+    connection.bind(socket_server)
+    connection.listen(1)
+
+    return connection
+
+def client_confirmation(connection):
+
+    connection, ip_cliente = connection.accept()
+    print(f"O cliente com endereço de IP f{ip_cliente[0]} aceitou.")
+    return connection, ip_cliente
+
+def close_connection(connection):
+
+    print("Terminando a conexao e saindo do programa...")
+    connection.close()
+
+def listening (connection, ip_cliente):
+    print("Começando o chat. Esperando por uma mensagem...")
+
+    while True:
+        rec_mensagem = connection.recv(BUFFER).decode("ascii")
+
+        if rec_mensagem != "":
+            print(f"\nCliente {ip_cliente[0]}: {rec_mensagem}")
+            if rec_mensagem == "sair":
+                print("O lado do cliente terminou a conexao. Digite 'sair' para terminar a conexao")
+
+        sending_mensagem = input("Servidor: ")
+
+        if sending_mensagem != "":
+            connection.send(bytes(sending_mensagem, "utf8"))
+            if sending_mensagem == "sair":
+                break
+
+    print("Saindo...")
+    close_connection(connection)
+
+
+
+if __name__ == '__main__':
+    conexao_atual = bind_to_the_server()
+    conexao_aceita, ip_cliente = client_confirmation(conexao_atual)
+    listening(conexao_aceita, ip_cliente)
+
+    sys.exit()
+
