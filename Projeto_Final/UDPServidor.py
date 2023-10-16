3bcdff96ff074ac94858da703a735476a0d36f17
Luiz Henrique <102159670+LuizHengs@users.noreply.github.com>
2023-10-16 13:01:51 -0300

    Conexão UDP

---
+++ Projeto_Final/UDPServidor.py
@@ -0,0 +1,51 @@
+import socket
+import sys
+
+SERVER_IP = ''
+SERVER_POT = 8000
+BUFFER = 1024
+
+def bind_to_the_server():
+
+    connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_server = (SERVER_IP, SERVER_POT)
+    connection.bind(socket_server)
+    #connection.listen(1)
+
+    return connection
+
+
+def close_connection(connection):
+
+    print("Terminando a conexao e saindo do programa...")
+    connection.close()
+
+def listening (connection):
+    print("Começando o chat. Esperando por uma mensagem...")
+
+    while True:
+        rec_mensagem, ip_cliente = connection.recvfrom(BUFFER)
+
+        if rec_mensagem != "":
+            print(f"\nCliente: {rec_mensagem.decode()}")
+            if rec_mensagem.decode() == "sair":
+                print("O lado do cliente terminou a conexao. Digite 'sair' para terminar a conexao")
+
+        sending_mensagem = input("Servidor: ")
+
+        if sending_mensagem != "":
+            connection.sendto(sending_mensagem.encode(), ip_cliente)
+            if sending_mensagem == "sair":
+                break
+
+    print("Saindo...")
+    close_connection(connection)
+
+
+
+if __name__ == '__main__':
+    conexao_atual = bind_to_the_server()
+    listening(conexao_atual)
+
+    sys.exit()
+
